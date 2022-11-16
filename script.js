97541b739e8d31ed86b79ae58ee3f1ebc2a63ea3
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-11-16 22:30:14 +0100

    Add PlayRound function

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ function GetComputerChoice() {
 	else
 		return "scissors";
 }
+
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+	
+	console.log(playerSelection, computerSelection)
+	
+	// rock and scissors
+	if (playerSelection === "rock" && computerSelection === "scissors")
+		console.log("You Win! Rock beats Scissors!");
+	else if (playerSelection === "scissors" && computerSelection === "rock")
+		console.log("You Lose! Rock beats Scissors!");
+
+	// scissors and paper
+	else if (playerSelection === "scissors" && computerSelection === "paper")
+		console.log("You Win! Scissors beat Paper!");
+	else if (playerSelection === "paper" && computerSelection === "scissors")
+		console.log("You Lose! Scissors beat Paper!");
+
+	// paper and rock
+	else if (playerSelection === "paper" && computerSelection === "rock")
+		console.log("You Win! Paper beats Rock!");
+	else if (playerSelection === "rock" && computerSelection === "paper")
+		console.log("You Lose! Paper beats Rock!");
+	
+	else
+		console.log("Tie!");
+}
