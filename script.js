a9999a78be5393a2b82016d33c51d8300b42a146
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-11-19 03:47:01 +0100

    Start and finish UI

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
 
 	//	rock and scissors
 	if (playerSelection === "rock" && computerSelection === "scissors")
@@ -43,29 +44,75 @@ function playRound(playerSelection, computerSelection) {
 function game() {
 	let playerWins = 0;
 	let computerWins = 0;
-	let possibleSelection = ["rock", "paper", "scissors"];
+	// let possibleSelection = ["rock", "paper", "scissors"];
 	
-	for (let i = 0; i < 5; ++i) {
-		// get input
-		let playerSelection = prompt("enter rock, paper or scissors").toLowerCase();
+	// for (let i = 0; i < 5; ++i) {
+	// 	// get input
+	// 	let playerSelection = prompt("enter rock, paper or scissors").toLowerCase();
 
-		// check for bad input
-		while (possibleSelection.indexOf(playerSelection) == -1) {
-			alert("Bad input!");
-			playerSelection = prompt("enter rock, paper or scissors").toLocaleLowerCase();
-		}
+	// 	// check for bad input
+	// 	while (possibleSelection.indexOf(playerSelection) == -1) {
+	// 		alert("Bad input!");
+	// 		playerSelection = prompt("enter rock, paper or scissors").toLocaleLowerCase();
+	// 	}
 
-		let result = playRound(playerSelection, getComputerChoice());
-		alert(result);
+	// 	let result = playRound(playerSelection, getComputerChoice());
+	// 	alert(result);
 		
-		// calculate score
+	// 	// calculate score
+	// 	if (result.toLowerCase().startsWith("you win!"))
+	// 		++playerWins;
+	// 	if (result.toLowerCase().startsWith("you lose!"))
+	// 		++computerWins;
+		
+	// 	console.log(playerWins, " : ", computerWins);
+	// }
+}
+
+function checkValue(e) {
+	console.log(this.textContent);
+}
+
+
+
+const resultDiv = document.querySelector("#result");
+const buttons = document.querySelectorAll("button");
+const score = document.querySelector("#score")
+const defaultResultStyle = score.style;
+let playerWins = 0;
+let computerWins = 0;
+
+buttons.forEach(button => {
+	button.addEventListener('click', () => {
+		const result = playRound(button.textContent, getComputerChoice());
+		// console.log(result);
+		
+		resultDiv.textContent = result;
+
 		if (result.toLowerCase().startsWith("you win!"))
 			++playerWins;
 		if (result.toLowerCase().startsWith("you lose!"))
 			++computerWins;
-		
-		console.log(playerWins, " : ", computerWins);
-	}
-}
 
-game();
+		score.textContent = `Score: ${playerWins} : ${computerWins}`;
+
+		if (computerWins === 5) {
+			score.textContent = '';
+			resultDiv.textContent = "YOU LOST!";
+			resultDiv.style = "color: rgb(255, 25, 25); font-weight:bold";
+			playerWins = 0;
+			computerWins = 0;
+		}
+		else if (playerWins === 5) {
+			score.textContent = '';
+			resultDiv.textContent = "YOU WON!"
+			resultDiv.style = "color: rgb(0, 255, 15); font-weight:bold";
+			playerWins = 0;
+			computerWins = 0;
+		}
+		
+		else {
+			resultDiv.style = defaultResultStyle;
+		}
+	});
+})
